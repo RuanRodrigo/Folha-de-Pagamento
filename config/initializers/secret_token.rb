aff1419e120fb0b360a946638cea32333d6c2c22
Gustavo Ramos <gustavo.ramos@veadvogados.com.br>
2013-01-29 22:56:59 -0200

    Creating the Rails Application
    From now on, everything *should* - well, *MUST* - be done on separate
    branches. Master will only have _ABSOLUTELY WORKING CODE_

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FolhaDePagamento::Application.config.secret_token = '50472673e86fde742bc006c03356d091862c563d095982ac8ab6ab0ceedfdcf8487424ad3e5d4e284f5208d402b1c7eca052a46ec446da6ca5e8764994587bee'
